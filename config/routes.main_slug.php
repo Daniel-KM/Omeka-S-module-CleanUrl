33a6ef5c1d0df8326b26c9e11c0009a776283abe
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-11-11 00:00:00 +0100

    Moved config in a specific file.

---
+++ config/routes.main_slug.php
@@ -0,0 +1,9 @@
+<?php
+namespace CleanUrl;
+
+// Copy this file in the main config folder, beside database.ini and local.config.php.
+
+// In order to have a main site without "/s/site-slug", fill your main site slug
+// here, so it won’t be used.
+// TODO Use the main default site slug from the settings.
+const MAIN_SITE_SLUG = false;
