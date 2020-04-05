d16fcc2c310b0f8b2ce87eb6878183980cbdf25d
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-04-06 00:00:00 +0200

    Cleaned config form.

--- Module.php
+++ Module.php
@@ -97,116 +97,6 @@ class Module extends AbstractModule
         $this->cacheItemSetsRegex();
     }
 
-    public function getConfigForm(PhpRenderer $renderer)
-    {
-        $services = $this->getServiceLocator();
-
-        $config = $services->get('Config');
-        $settings = $services->get('Omeka\Settings');
-
-        // The params are cached on load and save, to manage the case the user
-        // doesn‘t save the config.
-        $this->cacheCleanData();
-        $this->cacheItemSetsRegex();
-
-        // TODO Clean filling of the config form.
-        $data = [];
-        $defaultSettings = $config['cleanurl']['config'];
-        foreach ($defaultSettings as $name => $value) {
-            $data['clean_url_identifiers'][$name] = $settings->get($name, $value);
-            $data['clean_url_main_path'][$name] = $settings->get($name, $value);
-            $data['clean_url_item_sets'][$name] = $settings->get($name, $value);
-            $data['clean_url_items'][$name] = $settings->get($name, $value);
-            $data['clean_url_medias'][$name] = $settings->get($name, $value);
-            $data['clean_url_pages'][$name] = $settings->get($name, $value);
-            $data['clean_url_admin'][$name] = $settings->get($name, $value);
-        }
-
-        $form = $services->get('FormElementManager')->get(ConfigForm::class);
-        $form->init();
-        $form->setData($data);
-
-        $view = $renderer;
-        $translate = $view->plugin('translate');
-        $view->headStyle()->appendStyle('.inputs label { display: block; }');
-        $form->prepare();
-
-        $html = $translate('"CleanUrl" module allows to have clean, readable and search engine optimized Urls like http://example.com/my_item_set/item_identifier.') // @translate
-            . '<br />'
-            . sprintf($translate('See %s for more information.'), // @translate
-                sprintf('<a href="https://github.com/Daniel-KM/Omeka-S-module-CleanUrl">%s</a>', 'Readme'))
-            . '<br />'
-            . sprintf($translate('%sNote%s: identifiers should never contain reserved characters such "/" or "%%".'), '<strong>', '</strong>') // @translate
-            . '<br />'
-            . sprintf($translate('%sNote%s: For a good seo, it‘s not recommended to have multiple urls for the same resource.'), '<strong>', '</strong>') // @translate
-            . '<br />'
-            . $translate('To keep the original routes, the main site slug must be set in the file "clean_url.config.php".') // @translate
-            . $view->formCollection($form);
-        return $html;
-    }
-
-    public function handleConfigForm(AbstractController $controller)
-    {
-        $services = $this->getServiceLocator();
-        $config = $services->get('Config');
-        $settings = $services->get('Omeka\Settings');
-
-        $params = $controller->getRequest()->getPost();
-
-        $form = $services->get('FormElementManager')->get(ConfigForm::class);
-        $form->init();
-        $form->setData($params);
-        if (!$form->isValid()) {
-            $controller->messenger()->addErrors($form->getMessages());
-            return false;
-        }
-
-        // TODO Normalize the filling of the config form.
-        $params = array_merge(
-            $params['clean_url_identifiers'],
-            $params['clean_url_main_path'],
-            $params['clean_url_item_sets'],
-            $params['clean_url_items'],
-            $params['clean_url_medias'],
-            $params['clean_url_pages'],
-            $params['clean_url_admin']
-        );
-
-        // TODO Move the post-checks into the form.
-
-        // Sanitize first.
-        $params['cleanurl_identifier_prefix'] = trim($params['cleanurl_identifier_prefix']);
-        foreach ([
-            'cleanurl_main_path',
-            'cleanurl_item_set_generic',
-            'cleanurl_item_generic',
-            'cleanurl_media_generic',
-            'cleanurl_site_slug',
-            'cleanurl_page_slug',
-        ] as $posted) {
-            $value = trim(trim($params[$posted]), ' /');
-            $params[$posted] = mb_strlen($value) ? trim($value) . '/' : '';
-        }
-
-        $params['cleanurl_identifier_property'] = (int) $params['cleanurl_identifier_property'];
-
-        // The default url should be allowed for items and media.
-        $params['cleanurl_item_allowed'][] = $params['cleanurl_item_default'];
-        $params['cleanurl_item_allowed'] = array_values(array_unique($params['cleanurl_item_allowed']));
-        $params['cleanurl_media_allowed'][] = $params['cleanurl_media_default'];
-        $params['cleanurl_media_allowed'] = array_values(array_unique($params['cleanurl_media_allowed']));
-
-        $defaultSettings = $config['cleanurl']['config'];
-        $params = array_intersect_key($params, $defaultSettings);
-        foreach ($params as $name => $value) {
-            $settings->set($name, $value);
-        }
-
-        $this->cacheCleanData();
-        $this->cacheItemSetsRegex();
-        return true;
-    }
-
     /**
      * Add ACL rules for this module.
      */
@@ -219,6 +109,49 @@ class Module extends AbstractModule
             ->allow(null, [Controller\Site\CleanUrlController::class])
             ->allow($roles, [Controller\Admin\CleanUrlController::class])
         ;
+    }
+
+    /**
+     * Defines public routes "main_path / my_item_set | generic / dcterms:identifier".
+     *
+     * @todo Rechecks performance of routes definition.
+     */
+    protected function addRoutes()
+    {
+        $services = $this->getServiceLocator();
+        $router = $services->get('Router');
+        if (!$router instanceof \Zend\Router\Http\TreeRouteStack) {
+            return;
+        }
+
+        $settings = $services->get('Omeka\Settings');
+        $basePath = $services->get('ViewHelperManager')->get('basePath');
+
+        $router
+            ->addRoute('clean-url', [
+                'type' => \CleanUrl\Router\Http\CleanRoute::class,
+                // Check clean url first.
+                'priority' => 10,
+                'options' => [
+                    // TODO Save all these settings in one array.
+                    'base_path' => $basePath(),
+                    'settings' => [
+                        'default_site' => $settings->get('default_site'),
+                        'main_path' => $settings->get('cleanurl_main_path'),
+                        'item_set_generic' => $settings->get('cleanurl_item_set_generic'),
+                        'item_generic' => $settings->get('cleanurl_item_generic'),
+                        'media_generic' => $settings->get('cleanurl_media_generic'),
+                        'item_allowed' => $settings->get('cleanurl_item_allowed'),
+                        'media_allowed' => $settings->get('cleanurl_media_allowed'),
+                        'use_admin' => $settings->get('cleanurl_use_admin'),
+                        'item_set_regex' => $settings->get('cleanurl_item_set_regex'),
+                    ],
+                    'defaults' => [
+                        'controller' => 'CleanUrlController',
+                        'action' => 'index',
+                    ],
+                ],
+            ]);
     }
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
@@ -269,6 +202,117 @@ class Module extends AbstractModule
         );
     }
 
+    public function getConfigForm(PhpRenderer $renderer)
+    {
+        $services = $this->getServiceLocator();
+
+        $config = $services->get('Config');
+        $settings = $services->get('Omeka\Settings');
+
+        // The params are cached on load and save, to manage the case the user
+        // doesn‘t save the config.
+        $this->cacheCleanData();
+        $this->cacheItemSetsRegex();
+
+        // TODO Clean filling of the config form.
+        $data = [];
+        $defaultSettings = $config['cleanurl']['config'];
+        foreach ($defaultSettings as $name => $value) {
+            $data['clean_url_pages'][$name] = $settings->get($name, $value);
+            $data['clean_url_identifiers'][$name] = $settings->get($name, $value);
+            $data['clean_url_main_path'][$name] = $settings->get($name, $value);
+            $data['clean_url_item_sets'][$name] = $settings->get($name, $value);
+            $data['clean_url_items'][$name] = $settings->get($name, $value);
+            $data['clean_url_medias'][$name] = $settings->get($name, $value);
+            $data['clean_url_admin'][$name] = $settings->get($name, $value);
+        }
+
+        $form = $services->get('FormElementManager')->get(ConfigForm::class);
+        $form->init();
+        $form->setData($data);
+
+        $view = $renderer;
+        $translate = $view->plugin('translate');
+        $view->headStyle()->appendStyle('.inputs label { display: block; }');
+        $form->prepare();
+
+        $html = $translate('"CleanUrl" module allows to have clean, readable and search engine optimized urls for pages and resources, like http://example.com/my_item_set/item_identifier.') // @translate
+            . '<br />'
+            . sprintf($translate('See %s for more information.'), // @translate
+            sprintf('<a href="https://github.com/Daniel-KM/Omeka-S-module-CleanUrl">%s</a>', 'Readme'))
+            . '<br />'
+            . sprintf($translate('%sNote%s: identifiers should never contain reserved characters such "/" or "%%".'), '<strong>', '</strong>') // @translate
+            . '<br />'
+            . sprintf($translate('%sNote%s: For a good seo, it‘s not recommended to have multiple urls for the same resource.'), '<strong>', '</strong>') // @translate
+            . $view->formCollection($form);
+        return $html;
+    }
+
+    public function handleConfigForm(AbstractController $controller)
+    {
+        $services = $this->getServiceLocator();
+        $config = $services->get('Config');
+        $settings = $services->get('Omeka\Settings');
+
+        $params = $controller->getRequest()->getPost();
+
+        $form = $services->get('FormElementManager')->get(ConfigForm::class);
+        $form->init();
+        $form->setData($params);
+        if (!$form->isValid()) {
+            $controller->messenger()->addErrors($form->getMessages());
+            return false;
+        }
+
+        // TODO Normalize the filling of the config form.
+        $params = $form->getData();
+
+        // Make the params a flat array.
+        $params = array_merge(
+            $params['clean_url_pages'],
+            $params['clean_url_identifiers'],
+            $params['clean_url_main_path'],
+            $params['clean_url_item_sets'],
+            $params['clean_url_items'],
+            $params['clean_url_medias'],
+            $params['clean_url_admin']
+        );
+
+        // TODO Move the post-checks into the form.
+
+        // Sanitize first.
+        $params['cleanurl_identifier_prefix'] = trim($params['cleanurl_identifier_prefix']);
+        foreach ([
+            'cleanurl_main_path',
+            'cleanurl_item_set_generic',
+            'cleanurl_item_generic',
+            'cleanurl_media_generic',
+            'cleanurl_site_slug',
+            'cleanurl_page_slug',
+        ] as $posted) {
+            $value = trim(trim($params[$posted]), ' /');
+            $params[$posted] = mb_strlen($value) ? trim($value) . '/' : '';
+        }
+
+        $params['cleanurl_identifier_property'] = (int) $params['cleanurl_identifier_property'];
+
+        // The default url should be allowed for items and media.
+        $params['cleanurl_item_allowed'][] = $params['cleanurl_item_default'];
+        $params['cleanurl_item_allowed'] = array_values(array_unique($params['cleanurl_item_allowed']));
+        $params['cleanurl_media_allowed'][] = $params['cleanurl_media_default'];
+        $params['cleanurl_media_allowed'] = array_values(array_unique($params['cleanurl_media_allowed']));
+
+        $defaultSettings = $config['cleanurl']['config'];
+        $params = array_intersect_key($params, $defaultSettings);
+        foreach ($params as $name => $value) {
+            $settings->set($name, $value);
+        }
+
+        $this->cacheCleanData();
+        $this->cacheItemSetsRegex();
+        return true;
+    }
+
     /**
      * Display an identifier.
      */
@@ -305,49 +349,6 @@ class Module extends AbstractModule
             . '</h4><div class="value">'
             . ($identifier ?: '<em>' . $translator->translate('[none]') . '</em>')
             . '</div></div>';
-    }
-
-    /**
-     * Defines public routes "main_path / my_item_set | generic / dcterms:identifier".
-     *
-     * @todo Rechecks performance of routes definition.
-     */
-    protected function addRoutes()
-    {
-        $services = $this->getServiceLocator();
-        $router = $services->get('Router');
-        if (!$router instanceof \Zend\Router\Http\TreeRouteStack) {
-            return;
-        }
-
-        $settings = $services->get('Omeka\Settings');
-        $basePath = $services->get('ViewHelperManager')->get('basePath');
-
-        $router
-            ->addRoute('clean-url', [
-                'type' => \CleanUrl\Router\Http\CleanRoute::class,
-                // Check clean url first.
-                'priority' => 10,
-                'options' => [
-                    // TODO Save all these settings in one array.
-                    'base_path' => $basePath(),
-                    'settings' => [
-                        'default_site' => $settings->get('default_site'),
-                        'main_path' => $settings->get('cleanurl_main_path'),
-                        'item_set_generic' => $settings->get('cleanurl_item_set_generic'),
-                        'item_generic' => $settings->get('cleanurl_item_generic'),
-                        'media_generic' => $settings->get('cleanurl_media_generic'),
-                        'item_allowed' => $settings->get('cleanurl_item_allowed'),
-                        'media_allowed' => $settings->get('cleanurl_media_allowed'),
-                        'use_admin' => $settings->get('cleanurl_use_admin'),
-                        'item_set_regex' => $settings->get('cleanurl_item_set_regex'),
-                    ],
-                    'defaults' => [
-                        'controller' => 'CleanUrlController',
-                        'action' => 'index',
-                    ],
-                ],
-            ]);
     }
 
     /**
