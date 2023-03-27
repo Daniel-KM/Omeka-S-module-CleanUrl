8833d656b9ebc419b1a67c9a447e32258c85b353
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-03-27 00:00:00 +0000

    Added css-editor and some other paths in reserved slugs.

--- config/cleanurl.config.php
+++ config/cleanurl.config.php
@@ -15,7 +15,7 @@ namespace CleanUrl;
  * (admin, api, item, login, etc.).
  * It can be edited as needed. This regex pattern must start with a "|".
  */
-const SLUGS_RESERVED = '|access|adminer|annotation|ark|ark%3A|ark:|atom|auth|basket|bulk|bulk-check|bulk-import|bulk-export|cartography|collecting|comment|correction|cron|csvimport|custom-ontology|custom-vocab|download|ebook|edition|elastic|elastic-search|embed|embed-item|embed-page|epub|export|favorite|feed|find|graph|guest|iiif|iiif-img|iiif-search|iiif-server|image-server|import|ixif|ixif-media|ldap|login-admin|map|map-browse|ns|oai|oai-pmh|oauth|output|pdf|rss|saml|scripto|search|search-manager|selection|sitemap|solr|statistics|stats|story|storymap|subscription|tag|tagging|tags|timeline|unapi|upload|uri-dereferencer|value-suggest|vocab-suggest|xml-sitemap|zotero|zotero-import';
+const SLUGS_RESERVED = '|access|adminer|annotation|ark|ark%3A|ark:|atom|auth|basket|bulk|bulk-check|bulk-import|bulk-export|cartography|collecting|comment|correction|cron|css-editor|csvimport|custom-ontology|custom-vocab|download|ebook|edition|elastic|elastic-search|embed|embed-item|embed-page|epub|export|favorite|feed|find|graph|guest|iiif|iiif-img|iiif-search|iiif-server|image-server|import|ixif|ixif-media|ldap|login-admin|map|map-browse|ns|oai|oai-pmh|oauth|output|pdf|rss|saml|scripto|search|search-manager|selection|sitemap|solr|statistics|stats|story|storymap|subscription|tag|tagging|tags|timeline|unapi|upload|uri-dereferencer|value-annotation|value-suggest|vocab-suggest|xml-sitemap|zip|zotero|zotero-import';
 
 /**
  * @todo Remove the "s", that is not required, but kept for now for modules and themes that don't use helper url(). Anyway it's not working.
