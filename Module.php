a5fe45c792498a9e6981d040e619d9f5130c0805
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-10-28 00:00:00 +0000

    Updated Common version.

--- Module.php
+++ Module.php
@@ -91,10 +91,10 @@ class Module extends AbstractModule
         $plugins = $services->get('ControllerPluginManager');
         $translate = $plugins->get('translate');
 
-        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.62')) {
+        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.63')) {
             $message = new \Omeka\Stdlib\Message(
                 $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
-                'Common', '3.4.62'
+                'Common', '3.4.63'
             );
             throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
         }
